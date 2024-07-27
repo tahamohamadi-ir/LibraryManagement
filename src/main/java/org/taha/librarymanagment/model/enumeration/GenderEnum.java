8b0778edb587a4ce737631681e8f2573b7179253
tahamohamadi-ir <Havij1234@>
2024-07-27 17:53:39 +0330

    first initial

--- src/main/java/org/taha/librarymanagment/model/enumeration/GenderEnum.java
+++ src/main/java/org/taha/librarymanagment/model/enumeration/GenderEnum.java
@@ -1,4 +1,21 @@
 package org.taha.librarymanagment.model.enumeration;
 
+import lombok.Getter;
+
+@Getter
+
 public enum GenderEnum {
+
+    Male(1 , "مرد"),
+    Female(2 , "زن"),
+    None(0 , "نامشخص");
+
+    private final int code;
+    private final String desc;
+
+
+    GenderEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
 }
--- src/main/java/org/taha/librarymanagment/model/enumeration/MembershipStatusEnum.java
+++ src/main/java/org/taha/librarymanagment/model/enumeration/MembershipStatusEnum.java
@@ -1,4 +1,19 @@
 package org.taha.librarymanagment.model.enumeration;
 
 public enum MembershipStatusEnum {
+
+    Active(1 , "فعال"),
+    Inactive(2 , "غیرفعال"),
+    Suspended(3 , "معلق"),
+    Expired(4 , "منقضی"),
+    InProgress(5 , "در حال بررسی");
+
+
+    private final int code;
+    private final String desc;
+
+    MembershipStatusEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
 }
